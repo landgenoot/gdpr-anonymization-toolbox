ac19e079832d477ec17a9f1c53dc2e14a3943057
Daan Middendorp <github-d@landgenoot.com>
2018-07-02 15:42:01 +0200

    Add node-mondrian and overwrite find function

--- server/boot/generate-data.js
+++ server/boot/generate-data.js
@@ -8,33 +8,33 @@ const faker = require('faker')
  * @param {LoopbackServer} server
  */
 module.exports = function generateData (app) {
-  var ds = app.dataSources.mysql
-  ds.automigrate(async function () {
-    for (let i = 0; i < 3.5e6; i++) {
-      faker.locale = 'de'
-      let address = faker.address.streetAddress()
+  // var ds = app.dataSources.mysql
+  // ds.automigrate(async function () {
+  //   for (let i = 0; i < 3.5e6; i++) {
+  //     faker.locale = 'de'
+  //     let address = faker.address.streetAddress()
 
-      faker.locale = faker.helpers.randomize([
-        'de', 'de', 'de', 'de', 'de', 'de',
-        'tr', 'pl', 'nl', 'fr', 'es', 'cz'
-      ])
+  //     faker.locale = faker.helpers.randomize([
+  //       'de', 'de', 'de', 'de', 'de', 'de',
+  //       'tr', 'pl', 'nl', 'fr', 'es', 'cz'
+  //     ])
 
-      await app.models.Citizen.create({
-        SSN: faker.random.number({min: 1e8, max: 1e9 - 1}),
-        name: faker.name.findName(),
-        dateOfBirth: new Date(faker.date.past(50, new Date('Sat Sep 20 1992 21:35:02')).setHours(0, 0, 0, 0)),
-        streetAddress: address,
-        city: 'Berlin',
-        zip: faker.random.number({min: 10000, max: 14169}),
-        phoneNumber: faker.phone.phoneNumber(),
-        IBAN: faker.finance.iban(),
-        salary: faker.random.number({min: 2e4, max: 1e5}),
-        jobTitle: faker.name.jobTitle(),
-        nationality: faker.locale
-      })
-      if (i % 1000 === 0) {
-        console.log(Math.round(i / 3.5e4) + '%')
-      }
-    }
-  })
+  //     await app.models.Citizen.create({
+  //       SSN: faker.random.number({min: 1e8, max: 1e9 - 1}),
+  //       name: faker.name.findName(),
+  //       dateOfBirth: new Date(faker.date.past(50, new Date('Sat Sep 20 1992 21:35:02')).setHours(0, 0, 0, 0)),
+  //       streetAddress: address,
+  //       city: 'Berlin',
+  //       zip: faker.random.number({min: 10000, max: 14169}),
+  //       phoneNumber: faker.phone.phoneNumber(),
+  //       IBAN: faker.finance.iban(),
+  //       salary: faker.random.number({min: 2e4, max: 1e5}),
+  //       jobTitle: faker.name.jobTitle(),
+  //       nationality: faker.locale
+  //     })
+  //     if (i % 1000 === 0) {
+  //       console.log(Math.round(i / 3.5e4) + '%')
+  //     }
+  //   }
+  // })
 }
--- server/models/citizen.js
+++ server/models/citizen.js
@@ -1,5 +1,18 @@
 'use strict'
 
 module.exports = function (Citizen) {
-
+  /**
+   * Overwrite the current find function in order
+   * to do the anonymization
+   */
+  Citizen.on('dataSourceAttached', () => {
+    var originalFind = Citizen.find
+    Citizen.find = (filter, user, cb) => {
+      filter = filter || {}
+      filter.limit = 100
+      originalFind.call(this, filter, (err, result) => {
+        return cb(err, result)
+      })
+    }
+  })
 }

03cdc1b1730a09affd760dc1b0150c2615944060
Daan Middendorp <github-d@landgenoot.com>
2018-07-19 12:35:56 +0200

    Some demonstration changes

--- server/models/citizen.js
+++ server/models/citizen.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const mondrian = require("node-mondrian")
+const mondrian = require('node-mondrian')
 
 export default function (Citizen) {
   /**
@@ -15,8 +15,11 @@ export default function (Citizen) {
       let k = (filter.k > 3 && filter.k < 15) ? filter.k : 3
       let l = (filter.l > 3 && filter.l < 15) ? filter.l : 2
       originalFind.call(Citizen, filter, async (err, result) => {
+        if (result.length < 10) {
+          return cb('Not enough results', [])
+        }
         const attributes = Citizen.definition.properties
-        //const kanonymized = await mondrian.kAnonymity(result, attributes, k)
+        // const kanonymized = await mondrian.kAnonymity(result, attributes, k)
         const ldiversed = await mondrian.lDiversity(result, attributes, l)
         const shuffled = ldiversed.sort(() => Math.random() - 0.5)
         return cb(err, shuffled)
